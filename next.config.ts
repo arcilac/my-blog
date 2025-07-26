d3be501a3562f147dc9c6a9520381e91ccaa2477
Camila Arcila <cami@Camilas-MacBook-Air.local>
2025-07-26 17:08:04 -0500

    add packages

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/my-blog',
   trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: '/my-blog',
+  images: { unoptimized: true },
 }
-
 module.exports = nextConfig
